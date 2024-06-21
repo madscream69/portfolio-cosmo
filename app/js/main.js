f24eba58840de6c9387d06c54af3f8160f7eba0a
Dolbezhnik <krendelalenkin@gmail.com>
2024-06-21 20:16:12 +0300

    start elemnt conclusion 50% is done

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,8 @@ burgerBtn.addEventListener('click', () => {
         .classList.toggle('header__menu--open');
 });
 
-document.querySelector('footer__bottom-copy').innerHTML =
-    +new Date().getFullYear() + ' &copy; <a href="#">cosmologik.ru</a> ';
+// document.querySelector('footer__bottom-copy').innerHTML =
+//     +new Date().getFullYear() + ' &copy; <a href="#">cosmologik.ru</a> ';
 
 function myFunction() {
     document.getElementById('myDropdown').classList.toggle('show');
@@ -27,21 +27,38 @@ function getParent(el) {
 }
 
 // Закройте выпадающее меню, если пользователь щелкает за его пределами
-window.onclick = function (event) {
-    if (!event.target.matches('.article-items__dropbtn')) {
-        console.log('mino');
-        var dropdowns = document.getElementsByClassName(
-            'article-items__dropdown-content'
-        );
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-                document
-                    .querySelector('.article-items__dropbtn')
-                    .classList.remove('show-btn');
-            }
+// window.onclick = function (event) {
+//     if (!event.target.matches('.article-items__dropbtn')) {
+//         console.log('mino');
+//         var dropdowns = document.getElementsByClassName(
+//             'article-items__dropdown-content'
+//         );
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//                 document
+//                     .querySelector('.article-items__dropbtn')
+//                     .classList.remove('show-btn');
+//             }
+//         }
+//     }
+// };
+//потом надо будет сделать возможность убрать звезды
+document
+    .querySelector('.conclusion__star-box')
+    .addEventListener('click', function (e) {
+        let stars = document.querySelectorAll('.fa-star');
+        let rate = +e.target.dataset.rate;
+        for (let i = 0; i < rate; i++) {
+            // console.log(stars[i]);
+            stars[i].classList = '';
+            stars[i].classList = 'fa-solid fa-star checked';
         }
-    }
-};
+
+        // stars.forEach((item) => {
+        //     item.classList
+        //     console.log(item);
+        // });
+    });
